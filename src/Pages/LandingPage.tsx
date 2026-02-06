62991677a69b4f8c5344184423985ad8487d9f63
JayArch97 <jquintanilla@twrk.com>
2026-02-06 16:11:05 -0600

    udpated landing page for my domain, added typewrite effect

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,17 +1,65 @@
-import { useState, useEffect } from "react"
-import Typewriter from "../animations/Typewriter"
+import { useState, useEffect } from "react";
+import Typewriter from "../animations/Typewriter";
+import NeoLandingPage from "../assets/neo-landing-page.png";
 
-function LandingPage(){
+//Arbitrary text array
+const textArray: string[] = [
+  "Wake up, Neo...",
+  "The Matrix has you...",
+  "Follow the white rabbit",
+  "Knock, knock, Neo.",
+];
 
-const textArray = ["Wake up, User...", "The Matrix is after you", "Let's get started!"]
+function LandingPage() {
+  const [index, setIndex] = useState(0);
+  const text = textArray[index];
+  const delayMs = text.length * 100;
 
-return(
-    <div className="flex justify-center-safe items-center border-red border-4 w-screen h-screen">
-        <h1 className="text-3xl justify-self-center align-self-center
-         text-green-300"><Typewriter text={textArray[0]} delay={100} /></h1>
-    </div>
-    )
+  useEffect(() => {
+    if (index === textArray.length - 1) return;
+    const nextId = setTimeout(() => {
+      setIndex((i) => (i + 1) % textArray.length);
+    }, delayMs + 1000);
+
+    return () => {
+      clearTimeout(nextId);
+    };
+  }, [index]);
+
+  return (
+    <>
+    <img src={NeoLandingPage} className="fixed top-0 left-0 w-screen h-screen object-cover opacity-10 bg-transparent" />
+      <div className="flex justify-center-safe items-center border-red border-4 w-screen h-screen">
+        <h1
+          className="text-3xl justify-self-center align-self-center
+         text-green-300"
+        >
+          <Typewriter key={text} text={text} delay={100} />
+        </h1>
+      </div>
+      <button
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          bottom: "100px",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          cursor: "pointer",
+          border: "2px solid white",
+          padding: "10px 20px",
+          borderRadius: "5px",
+          width: "100px",
+            backgroundColor: "transparent",
+            color: "green",
+            fontSize: "16px",
+        }}
+      >
+        Skip
+      </button>
+    </>
+  );
 }
 
-
-export default LandingPage
+export default LandingPage;
