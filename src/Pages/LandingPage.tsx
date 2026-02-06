dd8910b53b2cd3af0d5e703bc6828c68ddb85358
JayArch97 <jquintanilla@twrk.com>
2026-02-06 13:42:30 -0600

    adding typewriter component

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,10 +1,14 @@
+import { useState, useEffect } from "react"
+import Typewriter from "../animations/Typewriter"
 
 function LandingPage(){
+
+const textArray = ["Wake up, User...", "The Matrix is after you", "Let's get started!"]
 
 return(
     <div className="flex justify-center-safe items-center border-red border-4 w-screen h-screen">
         <h1 className="text-3xl justify-self-center align-self-center
-         text-green-300">Wake up, User...</h1>
+         text-green-300"><Typewriter text={textArray[0]} delay={100} /></h1>
     </div>
     )
 }
---
+++ src/animations/Typewriter.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+type TypewriterProps = {
+  text: Array<string> | string;
+  delay: number;
+};
+
+const Typewriter: React.FC<TypewriterProps> = ({ text, delay }) => {
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+    if (currentIndex < text.length) {
+    const timeout = setTimeout(() => {
+      setCurrentText(prevText => prevText + text[currentIndex]);
+      setCurrentIndex(prevIndex => prevIndex + 1);
+    }, delay);
+
+    return () => clearTimeout(timeout);
+  }
+}, [currentIndex, delay, text]);
+
+
+  return <span>{currentText}</span>;
+};
+
+export default Typewriter;
